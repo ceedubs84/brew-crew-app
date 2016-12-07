ee5d089404e10ea938139ebb479b79e21c118aa6
Chris Woodside <christopherwoodside@gmail.com>
2016-12-06 20:03:58 -0600

    Added create action and create route

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -4,6 +4,21 @@ class BrewsController < ApplicationController
     render 'index.json.jbuilder'
   end
 
+  def create
+    @brew = Brew.new(
+      name: params[:name],
+      style: params[:style],
+      hop: params[:hop],
+      yeast: params[:yeast],
+      malts: params[:malts],
+      ibu: params[:ibu],
+      alcohol: params[:alcohol],
+      blg: params[:blg]
+    )
+    @brew.save
+    render 'show.json.jbuilder'
+  end
+
   def show
     @brew = Brew.find_by(id: params[:id])
     render 'show.json.jbuilder'
